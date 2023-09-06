bc194a75a4f36d959c51eec9ba26ddd5b3dd8c62
Emerlyy <vadim200339@gmail.com>
2023-09-06 21:02:18 +0300

    unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,9 @@
 import '@testing-library/jest-dom'
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import App from "./App"
-
-test('demo', () => {
-    expect(true).toBe(true)
-})
 
 test("Renders the main page", () => {
     render(<App />)
-    expect(true).toBeTruthy()
+    const table = screen.getByTestId('table');
+    expect(table).toBeInTheDocument();
 })
