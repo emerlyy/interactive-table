8992c7b78efe85d2775ed9aa6dd19cd8ccd9bfc9
Emerlyy <vadim200339@gmail.com>
2023-09-06 20:56:17 +0300

    created utility for sorting data in the table

---
+++ src/utilities/sortData/sortData.ts
@@ -0,0 +1,18 @@
+import { DataResponse, SortingDirection, SortingValue } from "../../types"
+
+export const sortData = (data: DataResponse, sortingValue: SortingValue, sortingDirection: SortingDirection) => {
+  if (sortingDirection === 'asc') {
+    return data.sort((a, b) => {
+      if (typeof a[sortingValue] === 'number') {
+        return +a[sortingValue] - +b[sortingValue]
+      } else return a[sortingValue].toString().localeCompare(b[sortingValue].toString())
+    })
+  }
+  else {
+    return data.sort((a, b) => {
+      if (typeof a[sortingValue] === 'number') {
+        return +b[sortingValue] - +a[sortingValue]
+      } else return -1 * a[sortingValue].toString().localeCompare(b[sortingValue].toString())
+    })
+  }
+}
