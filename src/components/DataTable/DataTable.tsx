60e38fd462a30abc7a0e98c89030134f4e7df553
Emerlyy <vadim200339@gmail.com>
2023-09-07 13:19:18 +0300

    changed column model, refactored w SortingButton

--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -1,34 +1,75 @@
-import { DataResponse, SortingValue } from "../../types";
+import { DataResponse, SortingDirection, SortingValue } from "../../types";
 import DataTableRow from "../DataTableRow/DataTableRow";
+import SortingButton from "../SortingButton/SortingButton";
+import styles from './DataTable.module.css';
 
 interface DataTableProps {
     data?: DataResponse,
+    sortingValue?: SortingValue,
+    sortingDirection?: SortingDirection,
     onSortingClick: (sortingValue: SortingValue) => void
 }
 
-const columns: SortingValue[] = ['id', 'firstName', 'lastName', 'email', 'phone'];
+type Column = {
+    label: string,
+    value: SortingValue
+}
 
-const DataTable = ({ data, onSortingClick }: DataTableProps) => {
+const columns: Column[] = [
+    {
+        label: 'Id',
+        value: 'id'
+    },
+    {
+        label: 'First Name',
+        value: 'firstName'
+    },
+    {
+        label: 'Last Name',
+        value: 'lastName'
+    },
+    {
+        label: 'Email',
+        value: 'email'
+    },
+    {
+        label: "Phone",
+        value: 'phone'
+    },
+]
+
+const DataTable = ({ data, sortingValue, sortingDirection, onSortingClick }: DataTableProps) => {
     return (
-        <table>
-            <thead>
+        <table className={styles.table}>
+            <thead className={styles.tableHeader}>
                 <tr>
-                    {columns.map(item => <th key={item}>
-                        <button onClick={() => onSortingClick(item)}>
-                            {item}
-                        </button>
-                    </th>)}
+                    {
+                        columns.map(
+                            item => {
+                                const isActive = item.value === sortingValue;
+                                console.log(isActive)
+                                return (
+                                    <th key={item.value} >
+                                        <SortingButton
+                                            label={item.label}
+                                            onButtonClick={() => onSortingClick(item.value)}
+                                            direction={isActive ? sortingDirection : undefined} />
+                                    </th>
+                                )
+                            }
+                        )
+                    }
                 </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
+                {/* <tr>
+                    <td><input /></td>
+                    <td><input /></td>
+                    <td><input /></td>
+                    <td><input /></td>
+                    <td><input /></td>
+                </tr> */}
             </thead>
             <tbody>
-                {data?.map(item => <DataTableRow key={item.phone} item={item} />)}
+                {data?.map(item => <DataTableRow styleClassName={styles.tableRow} key={item.phone} item={item} />)}
             </tbody>
         </table>
     )
