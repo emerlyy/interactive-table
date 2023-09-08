418a94c2007561aa65ddf0b15e9643735c268d71
Emerlyy <vadim200339@gmail.com>
2023-09-08 10:20:17 +0300

    added loading

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,6 +6,7 @@ import { sortData } from '../../utilities';
 import ClientPagination from '../ClientPagination/ClientPagination';
 import DataTable from '../DataTable/DataTable';
 import styles from './App.module.css';
+import Loading from '../Loading/Loading';
 
 const fetchData = async () => {
   const { data } = await axios.get('http://www.filltext.com/?rows=300&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&delay=3&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D');
@@ -35,7 +36,7 @@ function App() {
   }
 
   if (isLoading) {
-    return <h3>Loading..</h3>
+    return <Loading />
   }
 
   if (isError) {
