c525b4db5462fb5ee4a4e2db2c71342378bc3911
Emerlyy <vadim200339@gmail.com>
2023-09-10 22:14:38 +0300

    added a form to create elements

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,15 @@
 import axios from 'axios';
 import { useState } from 'react';
+import { SubmitHandler } from 'react-hook-form';
 import { useQuery } from 'react-query';
 import { SortingDirection, SortingValue } from '../../types';
 import { sortData } from '../../utilities';
+import AddElementForm, { FormInputs } from '../AddElementForm/AddElementForm';
 import ClientPagination from '../ClientPagination/ClientPagination';
 import DataTable from '../DataTable/DataTable';
-import styles from './App.module.css';
 import Loading from '../Loading/Loading';
+import styles from './App.module.css';
+
 
 const fetchData = async () => {
   const { data } = await axios.get('http://www.filltext.com/?rows=300&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&delay=3&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D');
@@ -35,6 +38,12 @@ function App() {
     }
   }
 
+  const addElement: SubmitHandler<FormInputs> = (element) => {
+    response.unshift(element);
+    setSortingValue(null);
+    setSortingDirection(null);
+  }
+
   if (isLoading) {
     return <Loading />
   }
@@ -55,6 +64,7 @@ function App() {
 
   return (
     <main className={styles.app}>
+      <AddElementForm onSubmit={addElement} />
       <ClientPagination data={showingData} dataPerPage={50}>
         <DataTable
           onSortingClick={changeSorting}
