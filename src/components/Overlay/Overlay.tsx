f9d2d8e3853aa5cbe8a51a13219447c815e3ca4d
Emerlyy <vadim200339@gmail.com>
2023-09-12 17:53:24 +0300

    created overlay component

---
+++ src/components/Overlay/Overlay.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+import styles from './Overlay.module.css';
+
+interface OverlayProps {
+    isOpen: boolean,
+    children: ReactNode
+}
+
+const Overlay = ({ isOpen, children }: OverlayProps) => {
+    return (
+        <>
+            {isOpen && (
+                <div className={styles.overlay}>
+                    {children}
+                </div>
+            )}
+        </>
+    )
+}
+
+export default Overlay;
