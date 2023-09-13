9b318fc9e68f48f4037a123e7a6a7606f1540577
Emerlyy <vadim200339@gmail.com>
2023-09-13 11:09:41 +0300

    changed tab size to 2

--- src/components/Overlay/Overlay.tsx
+++ src/components/Overlay/Overlay.tsx
@@ -2,20 +2,20 @@ import { ReactNode } from "react";
 import styles from './Overlay.module.css';
 
 interface OverlayProps {
-    isOpen: boolean,
-    children: ReactNode
+  isOpen: boolean,
+  children: ReactNode
 }
 
 const Overlay = ({ isOpen, children }: OverlayProps) => {
-    return (
-        <>
-            {isOpen && (
-                <div className={styles.overlay}>
-                    {children}
-                </div>
-            )}
-        </>
-    )
+  return (
+    <>
+      {isOpen && (
+        <div className={styles.overlay}>
+          {children}
+        </div>
+      )}
+    </>
+  )
 }
 
 export default Overlay;
